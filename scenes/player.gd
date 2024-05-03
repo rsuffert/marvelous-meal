6bd9cc1aec30c027d8ee2cd63ff45363d9bf644a
Gabriel-Tabajara <gataba.santos@gmail.com>
2024-05-03 18:54:59 -0300

    Chef Rato andando

---
+++ scenes/player.gd
@@ -0,0 +1,66 @@
+extends CharacterBody2D
+
+@export var speed = 150.0
+@onready var sprite = $AnimatedSprite2D
+
+#func _ready() -> void:
+#	sprite = $PlayerSprite
+	
+func get_8way_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	#print(input_direction)
+	velocity = input_direction * speed
+	
+#func get_side_input():
+	#velocity.x = 0
+	#var vel := Input.get_axis("left", "right")
+	#var jump := Input.is_action_just_pressed('jump')
+#
+	#if is_on_floor() and jump:		
+		#velocity.y = jump_speed
+		## Emitir o sinal "jumped"
+		## jumped.emit()
+		#get_tree().call_group("hud", "updateScore")
+		#var b : = box.instantiate()
+		#b.position = global_position
+		#owner.add_child(b)
+	#velocity.x = vel * speed
+	
+func animate():
+	if velocity.x > 0:		
+		sprite.play("right")
+	elif velocity.x < 0:
+		sprite.play("left")
+	elif velocity.y > 0:
+		sprite.play("down")
+	elif velocity.y < 0:
+		sprite.play("up")
+	else:
+		sprite.stop()		
+
+#func animate_side():
+	#if velocity.x > 0:
+		#sprite.play("right")
+	#elif velocity.x < 0:
+		#sprite.play("left")
+	#else:
+		#sprite.stop()
+	
+func move_8way(delta):
+	get_8way_input()
+	animate()
+	move_and_slide()
+	#var collision_info = move_and_collide(velocity * delta)
+	#if collision_info:
+		#velocity = velocity.bounce(collision_info.get_normal())
+		#move_and_collide(velocity * delta * 10)
+
+#func move_side(delta):
+	#velocity.y += gravity * delta
+	#get_side_input()
+	#animate_side()
+	#move_and_slide()
+	
+func _physics_process(delta):
+	move_8way(delta)
+	#move_side(delta)
