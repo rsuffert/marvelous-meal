9168b3ee53830b38623f391e33c324862ee31c10
Gabriel-Tabajara <gataba.santos@gmail.com>
2024-05-20 19:02:44 -0300

    adicionando icones e comecando pedidos

--- scenes/game.gd
+++ scenes/game.gd
@@ -6,20 +6,51 @@ extends Node2D
 @onready var time_label = $UserInterface/StatsContainer/TimeLabel
 @onready var orders_container = $UserInterface/OrdersContainer
 
+var heroes = ['deadpool', 'hulk', 'spider'];
+var dishes = [['Batata', 10], ['MacTudo', 30]]
+var orders = []
+var inuseHeroes = [];
+var rng = RandomNumberGenerator.new()
+
 func _ready() -> void:
 	score_label.text = "Score: " + str(current_score)
 	time_label.text = "Time: " + str(game_duration_seconds)
 
 func _on_timer_timeout() -> void:
+	incrementOrders()
 	if game_duration_seconds > 0:
 		game_duration_seconds -= 1
 		time_label.text = "Time: " + str(game_duration_seconds)
-		if game_duration_seconds % 10 == 0: # create an order every 10 seconds
-			create_order("res://assets/icons/free-spiderman-1502925-1273046.png", "res://assets/icons/4853280.png")		
+		if game_duration_seconds % 5 == 0 and len(heroes) > 0: # create an order every 10 seconds
+			create_order(getHero(), getDish())	
+			
 	else:
 		pass # change to game over scene
+		
+func getHero():
+	var pos = rng.randf_range(0, len(heroes))
+	var hero = heroes[pos]
+	inuseHeroes.append(hero)
+	heroes.remove_at(pos)	
+	return hero
+	
+func getDish():
+	var pos = rng.randf_range(0, len(dishes))
+	var dish = dishes[pos]
+	print(dish)
+	return dish
 
-func create_order(hero_texture_path, food_texture_path):
+func incrementOrders():
+	for order in orders:
+		order[3] += 1
+
+func create_order(hero, dish):
+	var hero_texture_path = "res://assets/icons/heroes/%s.happy.png" % hero
+	var food_texture_path = "res://assets/icons/dishes/%s.png" % dish[0]
+	
+	# nome do heroi, nome do prato, tempo do prato, tempo que está demorando para fazer
+	orders.append([hero, dish[0], dish[1], 0])
+	print(orders)
 	var panel = Panel.new()
 	panel.custom_minimum_size = Vector2(120,0)
 	
