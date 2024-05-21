3e71bceae450ec8aa8a2c574b4833ba874808b66
Ricardo Süffert <ricardo.suffert@edu.pucrs.br>
2024-05-21 00:56:45 -0300

    move code for deleting order into its function & add typing to vars, funcs and params

--- scenes/game.gd
+++ scenes/game.gd
@@ -10,10 +10,10 @@ const ORDERS_CREATION_INTERVAL = 10
 var rng = RandomNumberGenerator.new()
 
 # Listas para controle interno de pratos, herois e pedidos
-var heroes = ['deadpool', 'hulk', 'spider']
-var heroes_in_use = []
-var dishes = [['Batata', 10], ['MacTudo', 30]]
-var orders = []
+var heroes : Array[String] = ['deadpool', 'hulk', 'spider']
+var heroes_in_use : Array[String] = []
+var dishes : Array[Array] = [['Batata', 10], ['MacTudo', 30]]
+var orders : Array[Array] = []
 
 func _ready() -> void:
 	score_label.text = "Score: " + str(current_score)
@@ -34,46 +34,43 @@ func game_loop():
 		create_order(get_hero(), get_dish())
 
 # Gera um heroi aleatorio para ser adicionado a um pedido, retornando seu nome
-func get_hero():
-	var pos = rng.randf_range(0, len(heroes))
-	var hero = heroes[pos]
+func get_hero() -> String:
+	var pos : int = rng.randf_range(0, len(heroes))
+	var hero : String = heroes[pos]
 	heroes_in_use.append(hero)
 	heroes.remove_at(pos)	
 	return hero
 	
-# Gera um prato aleatorio para ser adicionado a um pedido, retornando seu nome e sua duracao
-func get_dish():
-	var pos = rng.randf_range(0, len(dishes))
-	var dish = dishes[pos]
+# Gera um prato aleatorio para ser adicionado a um pedido, retornando um array com o nome (string) e duracao (int) do pedido, nessa ordem
+func get_dish() -> Array:
+	var pos : int = rng.randf_range(0, len(dishes))
+	var dish : Array = dishes[pos]
 	return dish
 
 # Itera sobre os pedidos existentes e apaga aqueles cujo tempo acabou
-func check_existing_orders():
+func check_existing_orders() -> void:
 	for i in range(len(orders) - 1, -1, -1):
-		var order = orders[i]
+		var order : Array = orders[i]
 		order[3] += 1
 		if order[3] == order[2]: # tempo do pedido acabou
-			var hero = order[0]
-			heroes.append(hero)
-			heroes_in_use.remove_at(i)
-			orders.remove_at(i)
-			delete_order(i)
+			delete_order(order, i)
 
-func create_order(hero, dish):
-	var hero_texture_path = "res://assets/icons/heroes/%s.happy.png" % hero
-	var food_texture_path = "res://assets/icons/dishes/%s.png" % dish[0]
+# Cria um pedido dado um heroi e um prato (Array[string, int] (nome do prato e duracao)) e o adiciona a tela
+func create_order(hero: String, dish: Array) -> void:
+	var hero_texture_path : String = "res://assets/icons/heroes/%s.happy.png" % hero
+	var food_texture_path : String = "res://assets/icons/dishes/%s.png" % dish[0]
 	
 	# nome do heroi, nome do prato, tempo do prato, tempo que está demorando para fazer
 	orders.append([hero, dish[0], dish[1], 0])
 	var panel = Panel.new()
 	panel.custom_minimum_size = Vector2(120,0)
 	
-	var hero_icon = TextureRect.new()
+	var hero_icon : TextureRect = TextureRect.new()
 	hero_icon.size = Vector2(64, 64)
 	hero_icon.texture = load(hero_texture_path)
 	hero_icon.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
 	hero_icon.position = Vector2(0, 0)
-	var food_icon = TextureRect.new()
+	var food_icon : TextureRect = TextureRect.new()
 	food_icon.size = Vector2(64, 64)
 	food_icon.texture = load(food_texture_path)
 	food_icon.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
@@ -83,7 +80,12 @@ func create_order(hero, dish):
 	panel.add_child(food_icon)
 	orders_container.add_child(panel)
 
-func delete_order(index: int):
-	var child = orders_container.get_child(index)
+# Deleta um certo pedido (Array[String, String, int, int] (nome do heroi, nome do prato, tempo do prato, tempo que esta demorando para fazer))
+func delete_order(order : Array, index: int) -> void:
+	var hero : String = order[0]
+	heroes.append(hero)
+	heroes_in_use.remove_at(index)
+	orders.remove_at(index)
+	var child : Panel = orders_container.get_child(index)
 	orders_container.remove_child(child)
 	child.queue_free()
