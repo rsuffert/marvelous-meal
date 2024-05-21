ff1beea71de4572737fdae42fb8eec0601c8f43e
Ricardo Süffert <ricardo.suffert@edu.pucrs.br>
2024-05-21 14:52:17 -0300

    more refactorings

--- scenes/game.gd
+++ scenes/game.gd
@@ -11,25 +11,36 @@ var rng = RandomNumberGenerator.new()
 
 # Listas para controle interno de pratos, herois e pedidos
 var heroes : Array[String] = ['deadpool', 'hulk', 'spider']
-var heroes_in_use : Array[String] = []
-var dishes : Array[Array] = [['Batata', 10], ['MacTudo', 30]]
-var orders : Array[Array] = []
+var heroes_in_use : Array[String] = [] # evita que herois em uso aparecam fazendo um novo pedido
+var dishes : Array[Array] = [['Batata', 10], ['MacTudo', 30]] # [[nome do prato, tempo max de espera]]
+var orders : Array[Array] = [] # [[nome do heroi, nome do prato, tempo max de espera, tempo que passou]]
 
 func _ready() -> void:
 	score_label.text = "Score: " + str(current_score)
 	time_label.text = "Time: " + str(game_duration_seconds)
 
+# Chamada a cada segundo
 func _on_timer_timeout() -> void:
 	check_existing_orders()
 	if game_duration_seconds > 0:
 		game_loop()
 	else:
-		pass # change to game over scene
+		pass # TODO: mudar para a cena de gameover
 
-# Loop do jogo, com as acoes de atualizacao da UI e de coordenacao
+# Itera sobre os pedidos existentes e apaga aqueles cujo tempo acabou
+func check_existing_orders() -> void:
+	for i in range(len(orders) - 1, -1, -1): # itera de tras para frente
+		var order : Array = orders[i]
+		order[3] += 1
+		if order[3] == order[2]: # tempo do pedido acabou
+			current_score -= order[2]/3 # penalizar o jogador por nao entregar o pedido com a perda de 1/3 de sua duracao
+			delete_order(order, i)
+
+# Loop do jogo, com as acoes de atualizacao da UI e de coordenacao/controle do jogo
 func game_loop():
 	game_duration_seconds -= 1
 	time_label.text = "Time: " + str(game_duration_seconds)
+	score_label.text = "Score: " + str(current_score)
 	if game_duration_seconds % ORDERS_CREATION_INTERVAL == 0 and len(heroes) > 0:
 		create_order(get_hero(), get_dish())
 
@@ -38,7 +49,7 @@ func get_hero() -> String:
 	var pos : int = rng.randf_range(0, len(heroes))
 	var hero : String = heroes[pos]
 	heroes_in_use.append(hero)
-	heroes.remove_at(pos)	
+	heroes.remove_at(pos)
 	return hero
 	
 # Gera um prato aleatorio para ser adicionado a um pedido, retornando um array com o nome (string) e duracao (int) do pedido, nessa ordem
@@ -47,24 +58,20 @@ func get_dish() -> Array:
 	var dish : Array = dishes[pos]
 	return dish
 
-# Itera sobre os pedidos existentes e apaga aqueles cujo tempo acabou
-func check_existing_orders() -> void:
-	for i in range(len(orders) - 1, -1, -1):
-		var order : Array = orders[i]
-		order[3] += 1
-		if order[3] == order[2]: # tempo do pedido acabou
-			delete_order(order, i)
-
 # Cria um pedido dado um heroi e um prato (Array[string, int] (nome do prato e duracao)) e o adiciona a tela
 func create_order(hero: String, dish: Array) -> void:
+	# criar o pedido na lista de controle interno
+	orders.append([hero, dish[0], dish[1], 0]) # nome do heroi, nome do prato, tempo do prato, tempo que está demorando para fazer
+	
+	# gerar os caminhos das texturas do heroi e do prato p/ adicionar na UI
 	var hero_texture_path : String = "res://assets/icons/heroes/%s.happy.png" % hero
 	var food_texture_path : String = "res://assets/icons/dishes/%s.png" % dish[0]
 	
-	# nome do heroi, nome do prato, tempo do prato, tempo que está demorando para fazer
-	orders.append([hero, dish[0], dish[1], 0])
+	# criar o painel que vai conter as duas texturas na tela (fundo cinza)
 	var panel = Panel.new()
 	panel.custom_minimum_size = Vector2(120,0)
 	
+	# carregar as texturas em dois objetos TextureRect e arrumar os tamanhos, posicao no painel etc.
 	var hero_icon : TextureRect = TextureRect.new()
 	hero_icon.size = Vector2(64, 64)
 	hero_icon.texture = load(hero_texture_path)
@@ -76,16 +83,24 @@ func create_order(hero: String, dish: Array) -> void:
 	food_icon.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
 	food_icon.position = Vector2(64, 0)
 	
+	# adicionar as texturas carregadas no painel
 	panel.add_child(hero_icon)
 	panel.add_child(food_icon)
+	
+	# adicionar o painel na tela
 	orders_container.add_child(panel)
 
 # Deleta um certo pedido (Array[String, String, int, int] (nome do heroi, nome do prato, tempo do prato, tempo que esta demorando para fazer))
 func delete_order(order : Array, index: int) -> void:
+	# liberar o heroi em uso das listas internas
 	var hero : String = order[0]
 	heroes.append(hero)
 	heroes_in_use.remove_at(index)
+	
+	# remover o pedido da lista interna de controle
 	orders.remove_at(index)
+	
+	# remover o pedido da tela
 	var child : Panel = orders_container.get_child(index)
 	orders_container.remove_child(child)
 	child.queue_free()
