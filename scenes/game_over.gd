808fff5eda7c4b3a2559c406b6127ca2bacb7d70
Ricardo Süffert <ricardo.suffert@edu.pucrs.br>
2024-05-26 22:46:33 -0300

    add game over scene

--- scenes/game_over.gd
+++ scenes/game_over.gd
@@ -1,6 +1,14 @@
 extends Control
 
 @onready var punctuation_label = $ColorRect/CenterContainer/VBoxContainer/PunctutationLabel
+@export var game_scene_path : String = "res://scenes/game.tscn"
+
+func _process(delta : float) -> void:
+	if Input.is_action_just_released("ui_accept"):
+		var main_scene = load(game_scene_path).instantiate()
+		get_tree().root.add_child(main_scene)
+		get_tree().current_scene.queue_free()
+		get_tree().current_scene = main_scene
 
 func set_score(score: int) -> void:
 	punctuation_label.text = "Points:  " + str(score)
