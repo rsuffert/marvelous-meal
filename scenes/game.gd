d2af0b0b88bf245027d5e8507d10843e038e6414
Ricardo Süffert <ricardo.suffert@edu.pucrs.br>
2024-06-18 02:45:02 -0300

    code refactoring for clarity

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var rng = RandomNumberGenerator.new()
+
 ### REFERENCES TO UI COMPONENTS
 @onready var score_label = $UserInterface/StatsContainer/ScoreLabel
 @onready var time_label = $UserInterface/StatsContainer/TimeLabel
@@ -40,8 +42,7 @@ class Order:
 		self.dish = _dish
 		self.current_time = 0.0
 
-### INTERNAL VARIABLES
-var rng = RandomNumberGenerator.new()
+### STATE CONTROL VARIABLES
 const ORDERS_CREATION_INTERVAL : int = 10
 const MAX_WAVES : int = 3
 const DISH_TIME_REDUCTION_PERCENTAGE_PER_WAVE : float = 0.2
@@ -68,64 +69,44 @@ const PANELS_Y_COORDINATE : int = 95
 
 ### MAIN GAME FUNCTIONS
 func _ready() -> void:
-	# initialize button styles
-	normal_style = StyleBoxFlat.new()
-	normal_style.bg_color = Color(1, 1, 1, 0.5)  # Opaque white
-	normal_style.set_corner_radius_all(2)
-	normal_style.border_color = Color(0, 0, 0)
-	normal_style.set_border_width_all(2)
-	normal_style.set_content_margin_all(5)
-	selected_style = StyleBoxFlat.new()
-	selected_style.bg_color = Color(1, 0, 0, 0.5)  # Opaque red
-	selected_style.set_corner_radius_all(2)
-	selected_style.border_color = Color(1, 1, 1)
-	selected_style.set_border_width_all(2)
-	selected_style.set_content_margin_all(5)
-	# initialize score, time & wave labels
+	initialize_button_styles()
 	score_label.text = "Score:" + str(current_score)
 	time_label.text = "Time:" + str(remaining_time)
 	wave_label.text = "Wave: " + str(current_wave)
-	# initialize UI panels
 	initialize_ingredients_panel()
 	initialize_dishes_panel()
 	initialize_delivery_panel()
 	
-# Chamada a cada segundo
+# Called every second
 func _on_timer_timeout() -> void:
 	check_existing_orders()
 	if remaining_time > 0 and lives > 0:
 		game_loop()
-	else: # time's up. change to game over scene
-		var gameover_scene = load(scenes_dir_path + "game_over.tscn").instantiate()
-		gameover_scene.call_deferred("set_score", current_score)
-		var message : String = "Time's up!" if remaining_time <= 0 else "You have no lives left!"
-		gameover_scene.call_deferred("set_message", message)
-		get_tree().root.add_child(gameover_scene)
-		get_tree().current_scene.queue_free()
-		get_tree().current_scene = gameover_scene
+	else:
+		game_over()
 
-# Itera sobre os pedidos existentes e apaga aqueles cujo tempo acabou
+# Iterates over the existing orders and deletes those whose wait time are over
 func check_existing_orders() -> void:
-	for i in range(len(orders) - 1, -1, -1): # itera de tras para frente
+	for i in range(len(orders) - 1, -1, -1): # iterates backwards
 		var order : Order = orders[i]
 		order.current_time += 1
-		# atualizar imagem dos herois de acordo com o tempo transcorrido (humor)
-		if order.current_time / order.dish.time >= 2.0/3.0: # humor "bravo" (angry)
+		# updates heroes' images according to the elapsed time (humour)
+		if order.current_time / order.dish.time >= 2.0/3.0: # angry
 			var hero_texture : TextureRect = orders_container.get_child(i).get_child(0)
 			var new_hero_texture_path : String = heroes_dir_path + "%s.angry.png" % order.hero
 			hero_texture.texture = load(new_hero_texture_path)
 			hero_texture.tooltip_text = order.hero.capitalize() + " (angry)"
-		elif order.current_time / order.dish.time >= 1.0/3.0: # humor "normal"
+		elif order.current_time / order.dish.time >= 1.0/3.0: # normal
 			var hero_texture : TextureRect = orders_container.get_child(i).get_child(0)
 			var new_hero_texture_path : String = heroes_dir_path + "%s.normal.png" % order.hero
 			hero_texture.texture = load(new_hero_texture_path)
 			hero_texture.tooltip_text = order.hero.capitalize() + " (normal)"
-		if order.current_time == order.dish.time: # tempo do pedido acabou
+		if order.current_time == order.dish.time: # order wait time is over
 			lives -= 1
 			heart_bar.call_deferred("decrement_health")
 			delete_order(order, i)
 
-# Loop do jogo, com as acoes de atualizacao da UI e de coordenacao/controle do jogo
+# Game loop, handling updating the UI and coordinating/controlling the game
 func game_loop():
 	remaining_time -= 1
 	time_label.text = "Time:" + str(remaining_time)
@@ -138,29 +119,35 @@ func game_loop():
 		current_wave += 1
 		wave_label.text = "Wave: " + str(current_wave)
 
+# Handles terminating the game
+func game_over() -> void:
+	var gameover_scene = load(scenes_dir_path + "game_over.tscn").instantiate()
+	gameover_scene.call_deferred("set_score", current_score)
+	var message : String = "Time's up!" if remaining_time <= 0 else "You have no lives left!"
+	gameover_scene.call_deferred("set_message", message)
+	get_tree().root.add_child(gameover_scene)
+	get_tree().current_scene.queue_free()
+	get_tree().current_scene = gameover_scene
+
 ### CALLBACK FUNCTIONS FOR AREAS IN THE SCENARIO ENTERED
-# Called when a body enters the delivery area
 func _on_delivery_area_body_entered(body: Node2D) -> void:
 	if body == player and not orders.is_empty():
 		orders_container.visible = false
 		delivery_panel.visible = true
 
-# Called when a body leaves the delivery area
 func _on_delivery_area_body_exited(body: Node2D) -> void:
 	if body == player:
 		orders_container.visible = true
 		delivery_panel.visible = false
 
-# Called when a body enters the ingredients area
 func _on_ingredients_area_body_entered(body: Node2D) -> void:
 	if body == player:
 		ingredients_panel.visible = true
 
-# Called when a body leaves the ingredients area
 func _on_ingredients_area_body_exited(body: Node2D) -> void:
 	if body == player:
 		ingredients_panel.visible = false
-		
+
 func _on_preparation_area_body_entered(body: Node2D) -> void:
 	if body == player:
 		set_dishes_panel_buttons()
@@ -182,7 +169,7 @@ func _on_ingredient_button_pressed(button : Button) -> void:
 		button.add_theme_stylebox_override("normal", selected_style)
 		ingredients.append(ingredient)
 
-# Replaces the ingredients panel with the dishes panel
+# Updates the buttons of the dishes panel according to the dishes that can be prepared with the currently ingredients being held
 func set_dishes_panel_buttons() -> void:
 	# remove current plates being rendered
 	for child in dishes_buttons_container.get_children():
@@ -200,14 +187,13 @@ func set_dishes_panel_buttons() -> void:
 	dishes_panel.scale = Vector2(0.2,0.2)
 	dishes_panel.visible = true
 
-# Called when a possible dish button is pressed
 func _on_possible_dish_button_pressed(button: Button):
 	ingredients.clear()
 	reset_ingredients_buttons()
 	player.call('pickup_dish', button.icon, button.tooltip_text)
 	
-# Called when a hero button is pressed for delivery
 func _on_hero_delivery_button_pressed(button : Button):
+	# deliver current dish being held to the selected hero
 	for i in range(len(orders)):
 		var order = orders[i]
 		if order.dish.name == player.current_dish and button.tooltip_text.to_lower() == order.hero: # correct answer
@@ -215,11 +201,25 @@ func _on_hero_delivery_button_pressed(button : Button):
 			current_score += order.dish.time - order.current_time
 			delete_order(order, i)
 			return
-	# se chegar ate essa parte do codigo, clicou no heroi errado (penalizar com a perda de 10% da pontuação atual)
+	# if we get to this part of the code, the wrong hero has been clicked (player loses 10% of its current points)
 	if current_score > 0:
 		current_score -= current_score*0.1
 	
 ### FUNCIONS FOR MANAGING & INITIALIZING UI COMPONENTS
+func initialize_button_styles() -> void:
+	normal_style = StyleBoxFlat.new()
+	normal_style.bg_color = Color(1, 1, 1, 0.5)
+	normal_style.set_corner_radius_all(2)
+	normal_style.border_color = Color(0, 0, 0)
+	normal_style.set_border_width_all(2)
+	normal_style.set_content_margin_all(5)
+	selected_style = StyleBoxFlat.new()
+	selected_style.bg_color = Color(1, 0, 0, 0.5)
+	selected_style.set_corner_radius_all(2)
+	selected_style.border_color = Color(1, 1, 1)
+	selected_style.set_border_width_all(2)
+	selected_style.set_content_margin_all(5)
+
 func initialize_ingredients_panel() -> void:
 	ingredients_panel = Panel.new()
 	ingredients_panel.visible = false
@@ -288,15 +288,11 @@ func initialize_delivery_panel() -> void:
 	$UserInterface.add_child(delivery_panel)
 	
 # Cria um pedido dado um heroi e um prato e o adiciona a tela
+# Given a hero and a dish, creates an order, adding it to the screen and to the internal lists of the system to be tracked
 func create_order(hero: String, dish: Dish) -> void:
-	# criar o pedido na lista de controle interno
-	orders.append(Order.new(hero, dish)) # nome do heroi, nome do prato, tempo do prato, tempo que está demorando para fazer
-	
-	# gerar os caminhos das texturas do heroi e do prato p/ adicionar na UI
+	orders.append(Order.new(hero, dish))
 	var hero_texture_path : String = heroes_dir_path + "%s.happy.png" % hero
 	var food_texture_path : String = dishes_dir_path + "%s.png" % dish.name
-	
-	# carregar as texturas em dois objetos TextureRect e arrumar os tamanhos, posicao no painel etc.
 	var hero_icon : TextureRect = TextureRect.new()
 	hero_icon.size = Vector2(45, 45)
 	hero_icon.texture = load(hero_texture_path)
@@ -309,8 +305,6 @@ func create_order(hero: String, dish: Dish) -> void:
 	food_icon.tooltip_text = dish.name.capitalize()
 	food_icon.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
 	food_icon.position = Vector2(45, 0)
-	
-	# criar o painel que vai conter as duas texturas na tela
 	var panel = Panel.new()
 	panel.custom_minimum_size = Vector2(hero_icon.size.x + food_icon.size.x, max(hero_icon.size.y, food_icon.size.y))
 	panel.size_flags_vertical = Control.SIZE_SHRINK_CENTER
@@ -318,25 +312,16 @@ func create_order(hero: String, dish: Dish) -> void:
 	#stylebox.bg_color = Color(1, 1, 1, 0.5)
 	#stylebox.set_corner_radius_all(5)
 	#panel.add_theme_stylebox_override("panel", stylebox)
-	
-	# adicionar as texturas carregadas no painel
 	panel.add_child(hero_icon)
 	panel.add_child(food_icon)
-	
-	# adicionar o painel na tela
 	orders_container.add_child(panel)
 
-# Deleta um certo pedido
+# Deletes a given order, removing it from the screen and from the internal lists of the system
 func delete_order(order : Order, index: int) -> void:
-	# liberar o heroi em uso das listas internas
 	var hero : String = order.hero
 	heroes.append(hero)
 	heroes_in_use.remove_at(index)
-	
-	# remover o pedido da lista interna de controle
 	orders.remove_at(index)
-	
-	# remover o pedido da tela
 	var child : Panel = orders_container.get_child(index)
 	orders_container.remove_child(child)
 	child.queue_free()
@@ -348,8 +333,8 @@ func are_all_elements_present(list1: Array, list2: Array) -> bool:
 		if e not in list2:
 			return false
 	return true
-	
-# Gera um heroi aleatorio para ser adicionado a um pedido, retornando seu nome (o heroi gerado eh diferente do ultimo)
+
+# Generates a random hero to be added to an order, returning their name (the generated hero is different from the last generated hero)
 func get_hero() -> String:
 	var hero : String = last_hero
 	var pos : int
@@ -361,7 +346,7 @@ func get_hero() -> String:
 	heroes.remove_at(pos)
 	return hero
 	
-# Gera um prato aleatorio para ser adicionado a um pedido
+# Generates a random dish to be added to an order
 func get_dish() -> Dish:
 	var pos : int = rng.randi_range(0, len(dishes)-1)
 	var dish : Dish = dishes[pos]
